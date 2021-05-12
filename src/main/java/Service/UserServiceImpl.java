595dd54fa2340f9b645bc56614eac52e0339986b
DmitryDK3 <81352542+DmitryDK3@users.noreply.github.com>
2021-05-12 20:14:28 +0300

    Update UserServiceImpl.java

--- src/main/java/Service/UserServiceImpl.java
+++ src/main/java/Service/UserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
-    UserDao userDao;
+    private UserDao userDao;
 
     @Override
     public List<User> getUsers() {
